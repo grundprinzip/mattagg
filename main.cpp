048197e24aeff7ccfa96aaca0e8b5390c9ef3989
Martin Grund <grundprinzip@gmail.com>
2010-01-26 18:51:04 +0100

    * is working

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <boost/lexical_cast
+#include <boost/lexical_cast.hpp>
 
 #include "load.h"
 #include "trace.h"
@@ -20,11 +20,12 @@ int main(int argc, char** argv)
 	std::string fn(argv[1]);
 	std::cout << "Loading file: " << fn << std::endl;
 
-	size_t w = 
+	size_t w = boost::lexical_cast<size_t>(argv[2]);
+	size_t h = boost::lexical_cast<size_t>(argv[3]);
 
 	// Measure the loading time
 	Tracer trc;
-	TablePtr table = TableFactory::loadFile(fn, 3, 3, ColRead);
+	TablePtr table = TableFactory::loadFile(fn, w, h, ColRead);
 	Tracer::res_t tmp = trc();
 
 	std::cout << "Loaded table in (us): " << tmp << std::endl;
